c3c59bba59fc7526351212474d8c0912bb7d086b
Alex <roya.bunbun@gmail.com>
2026-01-22 18:10:36 -0500

    Create tob_bar_text.dart
    added tob bar custom widget

---
+++ lib/widgets/tob_bar_text.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+class top_bar extends StatelessWidget {
+  const top_bar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.5,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: const Color(0xFF4A3A2A),
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(32),
+          bottomRight: Radius.circular(32),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.5),
+            blurRadius: 20,
+            offset: Offset(0, 6),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Center(
+            child: Image.asset(
+              'lib/icons/WISERBITES.png',
+              height: MediaQuery.of(context).size.height * 0.1,
+              color: const Color(0xFFF5EDE2),
+            )
+            /*child: Text(
+            "AI Nutrition Assistant",
+            style: TextStyle(
+              color: const Color(0xFFF5EDE2),
+              fontSize: 50,
+              fontWeight: FontWeight.w600,
+            ),
+            ),*/
+          ),
+        ),
+      )
+    );
+  }
+}
